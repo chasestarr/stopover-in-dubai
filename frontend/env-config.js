b7884d398cc3af5294adc37071fc50ed8661282b
Chase Starr <chasestarr@gmail.com>
2018-08-26 13:24:15 -0700

    update front-end config

--- frontend/env-config.js
+++ frontend/env-config.js
@@ -2,6 +2,6 @@ const prod = process.env.NODE_ENV === 'production';
 
 module.exports = {
   'process.env.BACKEND_URL': prod
-    ? 'https://stopover-in-dubai.herokuapp.com:8080'
+    ? 'https://stopover-in-dubai.herokuapp.com'
     : 'http://localhost:8080',
 };
